771c9540c69ff94343d2bcf7b0fee0925a348dd2
izzatrahimov121 <7gqu0f7@code.edu.az>
2022-10-30 01:42:06 +0400

    created new file

---
+++ 29.10.2022/C#/Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region task1
+
+//int num = 2;
+//while (num > 2)
+//{
+//    num = num / 2;
+//}
+//if (num == 2)
+//{
+//    Console.WriteLine("eded 2nin quvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("eded 2nin quvveti deyil");
+//}
+
+#endregion
+
+#region task2
+
+//int number = 63336, numberClone;
+//numberClone=number;
+//int reverseNum = 0, num = 0;
+//while (number > 0)
+//{
+//    num = number % 10;
+//    reverseNum = reverseNum * 10 + num;
+//    number /= 10;
+//}
+//if (reverseNum==numberClone)
+//{
+//    Console.WriteLine("eded palindromdur");
+//}
+//else
+//{
+//    Console.WriteLine("eded palindrom deyil");
+//}
+
+#endregion
+
+#region task3
+//int num = 12;
+//int counter = 0;
+//for (int i = 1; i <= num; i++)
+//{
+//	if (num%i==0)
+//	{
+//		counter += 1;
+//	}
+//}
+//if (counter==2)
+//{
+//	Console.WriteLine("Eded sadedir");
+//}
+//else
+//{
+//	Console.WriteLine("eded murekkebdir");
+//}
+#endregion
+
+#region task4
+//string moonth = "January";
+//switch (moonth)
+//{
+//	case "December":
+//	case "January":
+//	case "February":
+//		Console.WriteLine("Winter");
+//		break;
+//    case "March":
+//    case "April":
+//    case "May":
+//        Console.WriteLine("Spring");
+//        break;
+//    case "June":
+//    case "July":
+//    case "August":
+//        Console.WriteLine("Summer");
+//        break;
+//    case "September":
+//    case "October":
+//    case "November":
+//        Console.WriteLine("Autumn");
+//        break;
+//    default:
+//        Console.WriteLine("Error");
+//		break;
+//}
+
+#endregion
---
+++ 29.10.2022/Js/tasks.js
@@ -0,0 +1,91 @@
+
+//#region Task1
+
+// let num = 64;
+// while (num>2)
+// {
+//    num = num / 2;
+// }
+// if (num==2)
+// {
+//    console.log("eded 2nin quvetidir");
+// }
+// else
+// {
+//    console.Log("eded 2nin quvveti deyil");
+// }
+
+//#endregion
+
+//#region Task2
+
+// let number = 63336, numberClone;
+// numberClone=number;
+// let reverseNum = 0, num = 0;
+// while (number > 0)
+// {
+//    num = number % 10;
+//    reverseNum = reverseNum * 10 + num;
+//    number /= 10;
+// }
+// if (reverseNum==numberClone)
+// {
+//    console.log("eded palindromdur");
+// }
+// else
+// {
+//    console.log("eded palindrom deyil");
+// }
+
+//#endregion
+
+//#region Task3
+// let num = 12;
+// let counter = 0;
+// for (let i = 1; i <= num; i++)
+// {
+// 	if (num%i==0)
+// 	{
+// 		counter += 1;
+// 	}
+// }
+// if (counter==2)
+// {
+// 	console.log("Eded sadedir");
+// }
+// else
+// {
+// 	console.log("eded murekkebdir");
+// }
+//#endregion
+
+//#region Task4
+// let moonth = "January";
+// switch (moonth)
+// {
+// 	case "December":
+// 	case "January":
+// 	case "February":
+// 		console.log("Winter");
+// 		break;
+//     case "March":
+//     case "April":
+//     case "May":
+//         console.log("Spring");
+//         break;
+//     case "June":
+//     case "July":
+//     case "August":
+//         console.log("Summer");
+//         break;
+//     case "September":
+//     case "October":
+//     case "november":
+//         console.log("Autumn");
+//         break;
+//     default:
+//         console.log("Error");
+// 		break;
+// }
+//#endregion
+
