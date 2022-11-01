73c9a79bec1f86f4dcd725218a48f918f20e09d7
izzatrahimov121 <7gqu0f7@code.edu.az>
2022-11-01 22:20:08 +0400

    New C# file

---
+++ 01.11.2022/Program.cs
@@ -0,0 +1,37 @@
+﻿#region task1
+//int[] arr = { -1, 34, 55, -44, 65 };
+//int avergceNum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    avergceNum += arr[i];
+//}
+//Console.WriteLine("ededi orta="+(double)avergceNum/arr.Length);
+#endregion
+
+#region task2
+//int[] arr = { 2, 3, 0, -3, 5, -1, 0, 7, -6 };
+//int positive = 0, negative = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > 0)
+//        positive++;//musbet ededler
+//    if (arr[i] < 0)
+//        negative++;//menfe ededler
+
+//}
+//Console.WriteLine("musbet ededler: "+positive);
+//Console.WriteLine("menfi ededler: "+negative);
+#endregion
+
+#region task3
+//int[] arr = { -2, 3, 4, 5, 6, 0, 32,64 };
+//int largeNum = arr[0];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//	if (arr[i]>largeNum)
+//	{
+//		largeNum = arr[i];
+//	}
+//}
+//Console.WriteLine(largeNum);
+#endregion
